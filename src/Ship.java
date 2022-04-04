1b685699d0790b845db1e14cb9bf5fbb3d038820
Diyor-Jaloldinov <49026397+Diyor-Jaloldinov@users.noreply.github.com>
2022-04-04 14:33:17 +0200

    commit adding some code

--- src/Ship.java
+++ src/Ship.java
@@ -1,4 +1,8 @@
 public class Ship {
+    private String Name,homePort,transportOrigin,destination;
+    private int ID,toxicContainers,heavyContainers,electricContainers,numberOfAllContainer,weightLoad;
+
+
 
 
 }
